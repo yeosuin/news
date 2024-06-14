a96e63e330e47b0af7c3b415f6786fe215c6cae4
yeosuin <immof@naver.com>
2024-06-14 10:43:51 +0900

    :sparkles: console 삭제, alret 창은 electron에서 오류 나기 때문에 modal로 변경

--- src/news.js
+++ src/news.js
@@ -3,45 +3,64 @@ let iconv  = require('iconv-lite');
 
 
 let content = document.getElementsByClassName('view')[0];
+let charDia = document.querySelector('.charDia');
+let nonDia = document.querySelector('.nonDia');
 const input = () => {
-	const url = document.getElementsByClassName('url')[0].value;
-	let requestOptions = {
-		method: "GET"
-		, uri: url
-		, headers: {"User-Agent": "Mozilla/5.0"}
-		, encoding: null
-	};
+	try {
+		const url = document.getElementsByClassName('url')[0].value;
+		if (url.length <= 0) {
+			nonDia.showModal();
+
+		}
+
+		let requestOptions = {
+			method: "GET"
+			, uri: url
+			, headers: {"User-Agent": "Mozilla/5.0"}
+			, encoding: null
+		};
 
 
 // request 모듈을 이용하여 html 요청
-	request(requestOptions, function (error, response, body) {
+		request(requestOptions, function (error, response, body) {
 			let charsetEuc = document.getElementsByClassName('charset')[0].value;
 			let strContents = new Buffer(body);
-		if(charsetEuc.includes('euc')) {
-			// 전달받은 결과를 EUC-KR로 디코딩하여 출력한다.
-			content.innerText = iconv.decode(strContents, 'EUC-KR').toString();
-		}else{
-			content.innerText = iconv.decode(strContents, 'UTF-8');
-		}
+			if (charsetEuc.includes('euc')) {
+				// 전달받은 결과를 EUC-KR로 디코딩하여 출력한다.
+				content.innerText = iconv.decode(strContents, 'EUC-KR').toString();
+			} else {
+				content.innerText = iconv.decode(strContents, 'UTF-8');
+			}
 
-		if((content.textContent).includes('euc-kr') && charsetEuc.includes('euc') === false) {
-			alert('charsest을 설정하세요');
-		}
-	});
+			if ((content.textContent).includes('euc-kr') && charsetEuc.includes('euc') === false) {
+				charDia.showModal();
+			}
+		});
+	}catch (error) {
+		console.log(error.message);
+	}
 }
+
+let close = document.querySelector('.close');
+close.addEventListener('click', (e) => {
+	charDia.close()
+})
+
+let close2 = document.querySelector('.close2');
+
+close2.addEventListener('click', (e) => {
+	nonDia.close()
+})
 
 const inputMath = () => {
 	const mathe_Reg = document.getElementsByClassName('match')[0].value;
-	document.getElementsByClassName('viewMath')[0].innerText = content.textContent.match(mathe_Reg)[0];
+	document.getElementsByClassName('viewMath')[0].innerText = RegExp(mathe_Reg).exec(content.textContent)[0];
 }
 
 const inputMath2 = () => {
 let regView = document.getElementsByClassName('viewMath')[0].defaultValue;
 	const mathExp = document.getElementsByClassName('match2')[0].value
-	console.log(regView)
-	console.log(mathExp)
 	let newExp = new RegExp(mathExp, "gi");
-	let abc = regView.replace(newExp,'');
-	console.log(abc)
-	document.getElementsByClassName('viewMath2')[0].innerText += abc;
+	let inputEXP = regView.replace(newExp,'');
+	document.getElementsByClassName('viewMath2')[0].innerText += inputEXP;
 }
